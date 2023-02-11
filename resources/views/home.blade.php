34326cd8f5b9e78a629403bdd55d80f09663c180
deivisjl <deivisjl@gmail.com>
2023-02-11 09:27:29 -0600

    Mostrando tipos de monedas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,34 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form action="#" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label for="">How much you want to pay?</label>
+                                <input  class="form-control"  value={{ mt_rand(500,100000) / 100}} type="number" name="value" min="5" step="0.01">
+                                <small class="form-text text-muted">
+                                    Use values with up to two decimal positions, using dots ","
+                                </small>
+                            </div>
+                            <div class="col-auto">
+                                <label for="">Currency</label>
+                                <select class="custom-select" name="currency" required>
+                                    @foreach ($currencies as $currency)
+                                        <option value="{{ $currency->iso}}">
+                                            {{ strtoupper($currency->iso)}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="text-center mt-3">
+                            <button type="submit" class="btn btn-primary btn-lg" id="payButton">Pay</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
