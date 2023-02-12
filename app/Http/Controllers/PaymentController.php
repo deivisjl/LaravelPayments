b17744567d02ece1373bdf770cdd9931ba206ec3
deivisjl <deivisjl@gmail.com>
2023-02-12 12:42:51 -0600

    Resolviendo servicio de metodo de pago

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Resolvers\PaymentPlatformResolver;
 use App\Services\PayPalService;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
+    protected $paymentPlatformResolver;
+
+    public function __construct(PaymentPlatformResolver $paymentPlatformResolver)
+    {
+        $this->middleware('auth');
+        $this->paymentPlatformResolver = $paymentPlatformResolver;
+    }
+
     public function pay(Request $request)
     {
         $rules = [
@@ -17,16 +26,26 @@ class PaymentController extends Controller
 
         $request->validate($rules);
 
-        $paymentPlatform = resolve(PayPalService::class);
+        $paymentPlatform = $this->paymentPlatformResolver
+                        ->resolveService($request->get('id'));
+
+        session()->put('paymentPlatformId', $request->payment_platform);
 
         return $paymentPlatform->handlePayment($request);
     }
 
     public function approval()
     {
-        $paymentPlatform = resolve(PayPalService::class);
+        if(session()->has('paymentPlatformId')){
+            $paymentPlatform = $this->paymentPlatformResolver
+                        ->resolveService(session()->get('paymentPlatformId'));
 
-        return $paymentPlatform->handleApproval();
+            return $paymentPlatform->handleApproval();
+        }
+
+        return redirect()
+            ->route('home')
+            ->withErrors('We cannot retrieve your payment platform. Try again, please.');
     }
 
     public function cancelled()
